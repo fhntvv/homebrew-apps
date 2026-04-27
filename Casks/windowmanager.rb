bd8404a25b9924ac4a3f1b9ccff6828a8cff110c
Artem Privalov <privalov.dev@gmail.com>
2026-04-27 21:15:53 +0200

    windowmanager 0.2.0

--- Casks/windowmanager.rb
+++ Casks/windowmanager.rb
@@ -1,6 +1,6 @@
 cask "windowmanager" do
-  version "0.1.0"
-  sha256 "28abecf5c120b1747db7a5c1204bfe891cfdd30966a34a6d3012e73be7f0d011"
+  version "0.2.0"
+  sha256 "edda44733a31795c15b31d5c3198ee357a0d16838bc0df81b0853b1ed0a674a7"
 
   url "https://github.com/fhntvv/window-manager/releases/download/v#{version}/WindowManager-#{version}.dmg"
   name "WindowManager"
